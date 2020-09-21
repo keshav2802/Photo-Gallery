4a0894a1ff524ea3bb704de339dca25bd5b38e1a
keshavism28 <keshavism28@gmail.com>
2020-09-21 17:09:54 +0530

    displayed the images in the grid and added the css styles for the grid

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -2,14 +2,19 @@ import React from 'react';
 import useFirestore from '../hooks/useFirestore';
 
 const ImageGrid = () => {
-  const {docs} = useFirestore('images');
-  console.log(docs);
-  
-  return(
-    <div className="imgage-grid">
-      images
-    </div>
-  )
-}
+	const { docs } = useFirestore('images');
+	console.log(docs);
+
+	return (
+		<div className='image-grid'>
+			{docs &&
+				docs.map((doc) => (
+					<div className='image-wrap' key={doc.id}>
+						<img src={doc.url} alt='uploaded pic' />
+					</div>
+				))}
+		</div>
+	);
+};
 
 export default ImageGrid;
