13837425bafd8e2e849b06776fed7c6455896e5f
keshavism28 <keshavism28@gmail.com>
2020-09-21 12:40:44 +0530

    initial commit

---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>Photo Gallery</h1>
+      <h2>Your Pictures</h2>
+      <p>Life sends you lots of colors. Keep them.</p>
+    </div>
+  )
+};
+
+export default Title;
